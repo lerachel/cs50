8e5ad582d978c1530c21f16e1349aa844c7834ab
lerachel <lerachel@uw.edu>
2020-08-22 19:48:15 -0700

    notfound.html and valid_entry.html

--- project1-wiki/encyclopedia/urls.py
+++ project1-wiki/encyclopedia/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:title>", views.check, name="check"),
+    path("valid_entry", views.check, name="valid_entry")
 ]
--- project1-wiki/encyclopedia/views.py
+++ project1-wiki/encyclopedia/views.py
@@ -2,9 +2,23 @@ from django.shortcuts import render
 
 from . import util
 
+entries = {"css", "django", "git", "html", "python"}
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def valid_entry(request, title):
+    return render(request, "encyclopedia/valid_entry.html", {
+        "entry": util.get_entry(title.capitalize())
+    })
+
+def not_found(request, title):
+    return render(request, "encyclopedia/notfound.html")
+
+def check(request, title):
+    if title in entries:
+        return valid_entry(request, title)
+    else:
+        return render(request, "encyclopedia/notfound.html")
